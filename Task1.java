c76719c3fb3404d417dd54a3ad4e9de48887f98d
se7en1337 <58147585+se7en1337@users.noreply.github.com>
2022-06-19 01:52:32 +0300

    src

---
+++ Task1.java
@@ -0,0 +1,58 @@
+package com.implemica;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * This class implements a fairly common
+ * a problem that leads to an example of solutions with
+ * using Catalan numbers
+ * @author Oleg Leonov
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter number");
+
+        // Variable initialization
+
+        int n = scn.nextInt();
+        System.out.println(String.format("Correct bracket expressions - %s",process(n)));
+    }
+
+    /**
+     * Checking the value and computing
+     * @param n - Entered number
+     * @return result of computing
+     */
+    public static int process(int n) {
+        int result = 0;
+        ArrayList<Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(1);
+
+        // Checking the value
+
+        if(n > 0)
+        {
+
+            // Computing
+
+            for(int i=0;i<n;i++)
+            {
+                result=0;
+                {
+                    for (int j = 0; j < l.size(); j++)
+                    {
+                        result += l.get(j) * l.get(l.size() - 1 - j);
+                    }
+                    l.add(result);
+                }
+            }
+        }
+        return l.get(l.size()-2);
+    }
+}
+
